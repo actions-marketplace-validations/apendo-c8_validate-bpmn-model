997d4d555e9defa5f8509e25965aee4fbaf2c6e5
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-08-15 13:51:46 +0200

    verified validate-bpmn.js is present

--- .github/workflows/validate-bpmn.js
+++ .github/workflows/validate-bpmn.js
@@ -3,22 +3,34 @@ const { execSync } = require('child_process');
 
 async function run() {
   try {
-    // Copy custom rules
-    execSync(`cp -r ${process.env.INPUT_CUSTOM_RULES_FOLDER}/* /opt/hostedtoolcache/node/18.17.0/x64/lib/node_modules/bpmnlint/rules/`, { stdio: 'inherit' });
+    // Copy custom rules test
+    execSync(
+      `cp -r ${process.env.INPUT_CUSTOM_RULES_FOLDER}/* /opt/hostedtoolcache/node/18.17.0/x64/lib/node_modules/bpmnlint/rules/`,
+      { stdio: 'inherit' }
+    );
 
     // List available rules
-    execSync('ls -al /opt/hostedtoolcache/node/18.17.0/x64/lib/node_modules/bpmnlint/rules', { stdio: 'inherit' });
+    execSync(
+      'ls -al /opt/hostedtoolcache/node/18.17.0/x64/lib/node_modules/bpmnlint/rules',
+      { stdio: 'inherit' }
+    );
 
     // Read and create .bpmnlintrc configuration file
-    const bpmnlintrcContents = fs.readFileSync(process.env.INPUT_BPMNLINTRC_PATH, 'utf8');
+    const bpmnlintrcContents = fs.readFileSync(
+      process.env.INPUT_BPMNLINTRC_PATH,
+      'utf8'
+    );
     fs.writeFileSync('.bpmnlintrc', bpmnlintrcContents);
 
     // Run BPMN validation and output result
     const bpmnFiles = fs.readdirSync(process.env.INPUT_BPMN_FILES_PATH);
-    bpmnFiles.forEach(file => {
+    bpmnFiles.forEach((file) => {
       if (file.endsWith('.bpmn')) {
         try {
-          const output = execSync(`bpmnlint ${process.env.INPUT_BPMN_FILES_PATH}/${file}`, { stdio: 'pipe' });
+          const output = execSync(
+            `bpmnlint ${process.env.INPUT_BPMN_FILES_PATH}/${file}`,
+            { stdio: 'pipe' }
+          );
           if (output.toString().trim().length === 0) {
             console.log(`No errors found in ${file}`);
           } else {
