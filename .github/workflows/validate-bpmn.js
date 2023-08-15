cea49c23ae53fe77d3aa84f4780dfa65e5ada910
Kristoffer Högberg <87658938+KristofferHogberg@users.noreply.github.com>
2023-08-15 13:34:42 +0200

    Update validate-bpmn.js

--- .github/workflows/validate-bpmn.js
+++ .github/workflows/validate-bpmn.js
@@ -1,30 +1,40 @@
-const { execSync } = require('child_process');
 const fs = require('fs');
-const path = require('path');
+const { execSync } = require('child_process');
 
-const bpmnFilesPath = process.env.INPUT_BPMN_FILES_PATH;
+async function run() {
+  try {
+    // Copy custom rules
+    execSync(`cp -r ${process.env.INPUT_CUSTOM_RULES_FOLDER}/* /opt/hostedtoolcache/node/18.17.0/x64/lib/node_modules/bpmnlint/rules/`, { stdio: 'inherit' });
 
-const bpmnLintCommand = 'bpmnlint';
+    // List available rules
+    execSync('ls -al /opt/hostedtoolcache/node/18.17.0/x64/lib/node_modules/bpmnlint/rules', { stdio: 'inherit' });
 
-try {
-  const files = fs.readdirSync(bpmnFilesPath);
-  for (const file of files) {
-    if (file.endsWith('.bpmn')) {
-      const filePath = path.join(bpmnFilesPath, file);
-      try {
-        const output = execSync(`${bpmnLintCommand} "${filePath}"`, { stdio: 'pipe' });
-        if (output.toString().trim().length === 0) {
-          console.log(`No errors found in ${file}`);
-        } else {
-          console.error(`Errors found in ${file}:`);
-          console.error(output.toString().trim());
+    // Read and create .bpmnlintrc configuration file
+    const bpmnlintrcContents = fs.readFileSync(process.env.INPUT_BPMNLINTRC_PATH, 'utf8');
+    fs.writeFileSync('.bpmnlintrc', bpmnlintrcContents);
+
+    // Run BPMN validation and output result
+    const bpmnFiles = fs.readdirSync(process.env.INPUT_BPMN_FILES_PATH);
+    bpmnFiles.forEach(file => {
+      if (file.endsWith('.bpmn')) {
+        try {
+          const output = execSync(`bpmnlint ${process.env.INPUT_BPMN_FILES_PATH}/${file}`, { stdio: 'pipe' });
+          if (output.toString().trim().length === 0) {
+            console.log(`No errors found in ${file}`);
+          } else {
+            console.error(`Errors found in ${file}:`);
+            console.error(output.toString().trim());
+          }
+        } catch (error) {
+          console.error(`Error running bpmnlint on ${file}:`);
+          console.error(error.stderr.toString().trim());
         }
-      } catch (error) {
-        console.error(`Error running bpmnlint on ${file}:`);
-        console.error(error.stderr.toString().trim());
       }
-    }
+    });
+  } catch (error) {
+    console.error('An error occurred:', error);
+    process.exit(1);
   }
-} catch (error) {
-  console.error('Error reading BPMN files:', error.message);
 }
+
+run();
